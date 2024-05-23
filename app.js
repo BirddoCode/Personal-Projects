396ccc900b0623de264097a0a8e77d269cdfdeaa
BirddoCode <143713055+BirddoCode@users.noreply.github.com>
2024-05-23 17:11:33 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+const prompt = require("prompt-sync")();
+
+let x;
+x = 3.14
+console.log(x);
+
+
+// length if a string
+// var FirstName;
+// var first_name = "lol";
+//FirstName = first_name.length;
+// console.log(FirstName)
+
+//function
+
+function LOl(i, g) {
+    var o;
+    o = i + g;
+    console.log(o);
+
+}
+
+LOl(2,5);
+
